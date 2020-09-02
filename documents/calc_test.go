b88e32bbf224ee384a0eee221b9ad1e244395665
chenbq011 <chenbq01@126.com>
2020-09-03 01:28:09 +0800

    hello

---
+++ documents/calc_test.go
@@ -0,0 +1,37 @@
+package documents
+
+import (
+	"fmt"
+)
+
+func ExampleAdd() {
+	result := Add(4, 2)
+	fmt.Println("4 + 2 =", result)
+
+	// Output:
+	// 4 + 2 = 6
+}
+
+func ExampleSub() {
+	result := Sub(4, 2)
+	fmt.Println("4 - 2 =", result)
+
+	// Output:
+	// 4 - 2 = 2
+}
+
+func ExampleMul() {
+	result := Mul(4, 2)
+	fmt.Println("4 * 2 =", result)
+
+	// Output:
+	// 4 * 2 = 8
+}
+
+func ExampleDiv() {
+	result := Div(4, 2)
+	fmt.Println("4 / 2 =", result)
+
+	// Output:
+	// 4 / 2 = 2
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module awesomeProject2
+
+go 1.15
+
+require (
+	github.com/hashicorp/raft v1.1.2 // indirect
+	golang.org/x/tools v0.0.0-20200902012652-d1954cc86c82 // indirect
+)
